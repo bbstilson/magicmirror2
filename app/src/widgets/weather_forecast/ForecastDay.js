79d4c7ce83ed3f3153e6106d86385494433af774
Brandon Stilson <bbstilson@fastmail.com>
2018-10-14 16:35:02 -0700

    style fixes. dynamic location fetching for weather

--- app/src/widgets/weather_forecast/ForecastDay.js
+++ app/src/widgets/weather_forecast/ForecastDay.js
@@ -11,7 +11,7 @@ export default class ForecastDay extends React.Component<Forecast> {
     return (
       <tr>
         <td className="weather-forecast__data">{moment.unix(time).format('ddd')}</td>
-        <td className="weather-forecast__data">
+        <td className="weather-forecast__data weather-forecast__icon">
           <Skycon icon={icon} color="white" />
         </td>
         <td className="weather-forecast__data">{parseInt(temperatureMax, 10)}°</td>
